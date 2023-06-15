7a4694bfe59db36904e715295554ee9af613c3c9
Carlos Carrete <carloscarrete,sc@gmail.com>
2023-06-15 01:13:08 -0600

    modal added and fixed some stuff

--- src/crypto/pages/CryptoPage.jsx
+++ src/crypto/pages/CryptoPage.jsx
@@ -9,6 +9,8 @@ export const CryptoPage = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [searchCoin, setSearchCoin] = useState('');
     const [modalOpen, setModalOpen] = useState(false);
+    const [currentCrypto, setCurrentCrypto] = useState();
+
 
     useEffect(() => {
         /*  setIsLoading(true);
@@ -36,8 +38,10 @@ export const CryptoPage = () => {
         return coins.name.toLowerCase().includes(searchCoin.toLowerCase());
     })
 
-    const handleOpenModal = () => {
+    const handleOpenModal = (e) => {
+        console.log(e)
         setModalOpen(true);
+        setCurrentCrypto(e);
     };
 
     const handleCloseModal = () => {
@@ -84,7 +88,7 @@ export const CryptoPage = () => {
                         <tbody>
                             {
                                 filteredCoins.map((coins) => (
-                                    <tr key={coins.id} onClick={handleOpenModal}>
+                                    <tr key={coins.id} onClick={() => handleOpenModal(coins)}>
                                         <td>
                                             <img className='table__img' src={coins.image} alt={coins.name} />
                                         </td>
@@ -108,13 +112,84 @@ export const CryptoPage = () => {
             }
 
             {modalOpen && (
-                    <div className="modal-container">
-                        <div className="modal-content">
+                <div className="modal-container">
+                    {/*  <div className="modal-content">
                             <h2>Modal</h2>
                             <p>Contenido del modal...</p>
                             <button onClick={handleCloseModal}>Cerrar Modal</button>
+                        </div> */}
+                    <div className='modal-content'>
+                        <img className='modal-img' src={currentCrypto.image} alt={currentCrypto.id} />
+                        <div className='modal-pre-info'>
+                            <div className='modal-info'>
+                                <h5>Last Updated: </h5>
+                                <h5 className='xd'>{currentCrypto.last_updated}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Market Capitalization: </h5>
+                                <h5 className='xd'>{currentCrypto.market_cap.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Circulating Supply: </h5>
+                                <h5 className='xd'>{currentCrypto.circulating_supply.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Current Price: </h5>
+                                <h5 className='xd'>{currentCrypto.current_price.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Fully Diluted Valuation: </h5>
+                                <h5 className='xd'>{currentCrypto.fully_diluted_valuation.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>24h High: </h5>
+                                <h5 className='xd'>{currentCrypto.high_24h.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>24h Low: </h5>
+                                <h5 className='xd'>{currentCrypto.low_24h.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Market Cap Change in 24h: </h5>
+                                <h5 className='xd'>{currentCrypto.market_cap_change_24h.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Market Cap Change Percentage in 24h: </h5>
+                                <h5 className='xd'>{currentCrypto.market_cap_change_percentage_24h}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Market Cap Rank: </h5>
+                                <h5 className='xd'>{currentCrypto.market_cap_rank}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Name: </h5>
+                                <h5 className='xd'>{currentCrypto.name}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Price Change in 24h: </h5>
+                                <h5 className='xd'>{currentCrypto.price_change_24h.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Price Change Percentage in 24h: </h5>
+                                <h5 className='xd'>{currentCrypto.price_change_percentage_24h}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Symbol: </h5>
+                                <h5 className='xd'>{currentCrypto.symbol}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Total Supply: </h5>
+                                <h5 className='xd'>{currentCrypto.total_supply.toLocaleString()}</h5>
+                            </div>
+                            <div className='modal-info'>
+                                <h5>Total Volume: </h5>
+                                <h5 className='xd'>{currentCrypto.total_volume.toLocaleString()}</h5>
+                            </div>
                         </div>
+                        <button onClick={handleCloseModal}>Cerrar Modal</button>
                     </div>
+
+                </div>
             )}
         </>
     )
